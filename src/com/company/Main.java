0a8e45e5a9dc72bc0a242fe1af287ca3a57a61b4
User <zamirbekovahadica@gmail.com>
2022-02-17 12:04:17 +0600

    Тестовое задание калькулятор Java.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -62,7 +62,12 @@ public class Main {
         }
         static String convertNumToRoman ( int numRim){
             String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV"
-                    , "XVI", "XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI"};
+                    , "XVI", "XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV"
+                    ,"XXV","XXVI","XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII",
+                    "XXXIX","XL","XLI"," XLII"," XLIII "," XLIV"," XLV","XLVI","XLVII "," XLVIII","XLIX", "L"
+
+
+            };
             final String s = roman[numRim];
             return s;
         }
